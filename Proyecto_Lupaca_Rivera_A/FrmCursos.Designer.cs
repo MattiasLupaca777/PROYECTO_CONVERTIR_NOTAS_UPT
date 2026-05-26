f4d3316265a6965b2e8976195a6e817ca36a9281
mattias <ml2024016553@virtual.upt.pe>
2026-05-26 06:05:15 -0500

    version-2-Diseño_retoques

--- Proyecto_Lupaca_Rivera_A/FrmCursos.Designer.cs
+++ Proyecto_Lupaca_Rivera_A/FrmCursos.Designer.cs
@@ -98,6 +98,7 @@
             this.btnAgregar.TabIndex = 5;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // btnModificar
             // 
@@ -152,7 +153,7 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(256, 77);
+            this.dataGridView1.Location = new System.Drawing.Point(220, 77);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(240, 303);
--- Proyecto_Lupaca_Rivera_A/FrmCursos.cs
+++ Proyecto_Lupaca_Rivera_A/FrmCursos.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Proyecto_Lupaca_Rivera_A
 {
     public partial class FrmCursos : Form
     {
+        ArchivoCursos archivo = new ArchivoCursos();
+
+        List<Curso> lista = new List<Curso>();
         public FrmCursos()
         {
             InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
