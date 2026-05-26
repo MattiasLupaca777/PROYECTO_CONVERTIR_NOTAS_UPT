a0069e9895100ff4e2de5c9c091ff8286bff0ac1
mattias <ml2024016553@virtual.upt.pe>
2026-05-25 22:53:58 -0500

    version 1-diseño completado

---
+++ Proyecto_Lupaca_Rivera_A/FrmConsultas.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Proyecto_Lupaca_Rivera_A
+{
+    partial class FrmConsultas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cboPais = new System.Windows.Forms.ComboBox();
+            this.cboUniversidad = new System.Windows.Forms.ComboBox();
+            this.cboSemestre = new System.Windows.Forms.ComboBox();
+            this.nudAnio = new System.Windows.Forms.NumericUpDown();
+            this.txtEstudiante = new System.Windows.Forms.TextBox();
+            this.dgvConsultas = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAnio)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsultas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(146, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FILTRAR POR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Pais";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Universidad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Año";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(63, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Semestre";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(63, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Estudiante";
+            // 
+            // cboPais
+            // 
+            this.cboPais.FormattingEnabled = true;
+            this.cboPais.Location = new System.Drawing.Point(190, 73);
+            this.cboPais.Name = "cboPais";
+            this.cboPais.Size = new System.Drawing.Size(121, 21);
+            this.cboPais.TabIndex = 6;
+            // 
+            // cboUniversidad
+            // 
+            this.cboUniversidad.FormattingEnabled = true;
+            this.cboUniversidad.Location = new System.Drawing.Point(190, 115);
+            this.cboUniversidad.Name = "cboUniversidad";
+            this.cboUniversidad.Size = new System.Drawing.Size(121, 21);
+            this.cboUniversidad.TabIndex = 7;
+            // 
+            // cboSemestre
+            // 
+            this.cboSemestre.FormattingEnabled = true;
+            this.cboSemestre.Location = new System.Drawing.Point(190, 188);
+            this.cboSemestre.Name = "cboSemestre";
+            this.cboSemestre.Size = new System.Drawing.Size(121, 21);
+            this.cboSemestre.TabIndex = 9;
+            // 
+            // nudAnio
+            // 
+            this.nudAnio.Location = new System.Drawing.Point(191, 149);
+            this.nudAnio.Name = "nudAnio";
+            this.nudAnio.Size = new System.Drawing.Size(120, 20);
+            this.nudAnio.TabIndex = 10;
+            // 
+            // txtEstudiante
+            // 
+            this.txtEstudiante.Location = new System.Drawing.Point(190, 220);
+            this.txtEstudiante.Name = "txtEstudiante";
+            this.txtEstudiante.Size = new System.Drawing.Size(121, 20);
+            this.txtEstudiante.TabIndex = 11;
+            // 
+            // dgvConsultas
+            // 
+            this.dgvConsultas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvConsultas.Location = new System.Drawing.Point(66, 263);
+            this.dgvConsultas.Name = "dgvConsultas";
+            this.dgvConsultas.Size = new System.Drawing.Size(240, 150);
+            this.dgvConsultas.TabIndex = 12;
+            // 
+            // FrmConsultas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvConsultas);
+            this.Controls.Add(this.txtEstudiante);
+            this.Controls.Add(this.nudAnio);
+            this.Controls.Add(this.cboSemestre);
+            this.Controls.Add(this.cboUniversidad);
+            this.Controls.Add(this.cboPais);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmConsultas";
+            this.Text = "FrmConsultas";
+            ((System.ComponentModel.ISupportInitialize)(this.nudAnio)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsultas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cboPais;
+        private System.Windows.Forms.ComboBox cboUniversidad;
+        private System.Windows.Forms.ComboBox cboSemestre;
+        private System.Windows.Forms.NumericUpDown nudAnio;
+        private System.Windows.Forms.TextBox txtEstudiante;
+        private System.Windows.Forms.DataGridView dgvConsultas;
+    }
+}
